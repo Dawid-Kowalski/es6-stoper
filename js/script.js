3842fd7569b90c1ee98e4d5af4a679377877c433
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-21 12:10:17 +0100

    Add start and restet function

--- js/script.js
+++ js/script.js
@@ -21,6 +21,36 @@ class Timer {
 	format(times) {
 		return `${add0(times.minutes)}:${add0(times.seconds)}:${add0(times.miliseconds)}`
 	}
+
+	start() {
+		if(!this.running) {
+			this.running = true;
+			this.watch = setInterval(() => this.step(), 10);
+		}
+	}
+
+	step() {
+		if(!this.running) return;
+		this.calculate();
+		this.print();
+	}
+
+	calculate() {
+	    this.times.miliseconds += 1;
+	    if (this.times.miliseconds >= 100) {
+	        this.times.seconds += 1;
+	        this.times.miliseconds = 0;
+	    }
+	    if (this.times.seconds >= 60) {
+	        this.times.minutes += 1;
+	        this.times.seconds = 0;
+	    }
+	}
+
+	stop() {
+    	this.running = false;
+    	clearInterval(this.watch);
+	}
 }
 
 function add0(value) {
@@ -34,8 +64,8 @@ function add0(value) {
 const timer = new Timer (document.querySelector(".timer"));
 
 let startButton = document.getElementById("start");
-startButton.addEventListener("click", () => Timer.start());
+startButton.addEventListener("click", () => timer.start());
 
 let stopButton = document.getElementById("stop");
-stopButton.addEventListener("click", () => Timer.stop());
+stopButton.addEventListener("click", () => timer.stop());
 
